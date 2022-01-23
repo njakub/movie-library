d3457eb6ce5853c0e510217ae860805d63cafc7f
Jakub Nicewicz <njakub94@gmail.com>
2022-01-23 18:34:52 +0000

    more changes

--- client/src/components/AppShell/AppShell.js
+++ client/src/components/AppShell/AppShell.js
@@ -8,7 +8,7 @@ function AppShell({ children }) {
       <Header />
       <main>
         <div className="px-4 mx-auto max-w-7xl sm:px-6">
-          <div className="py-4  md:justify-start md:space-x-10">{children}</div>
+          <div className="py-4 md:justify-start md:space-x-10">{children}</div>
         </div>
       </main>
     </>
