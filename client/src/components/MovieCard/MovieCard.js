c0f4ff8850d510625d4b7f5fa0f4153f395e1d0f
Jakub Nicewicz <njakub94@gmail.com>
2022-04-12 17:46:07 +0100

    added movie detail page

--- client/src/components/MovieCard/MovieCard.js
+++ client/src/components/MovieCard/MovieCard.js
@@ -1,32 +1,48 @@
 import RatingsComponent from "../RatingsComponent/RatingsComponent.js";
 import { searchById, searchByTitle, getByTitle } from "../../api/omdbApi";
 import { useQuery } from "react-query";
+import { useNavigate } from "react-router-dom";
 
 function MovieCard({ movie }) {
-  const { Title, Poster, Year, imdbID } = movie;
-
-  const {
-    isLoading,
-    error,
-    data: movieDetails,
-  } = useQuery(["searchById", imdbID], () => searchById(imdbID), {
-    staleTime: Infinity,
-    cacheTime: Infinity,
-  });
-
-  console.log("movieDetails", movieDetails);
+  const { title, description, imDbRating, image, imDbRatingVotes, id } = movie;
+  let navigate = useNavigate();
+  const handleClickView = () =>
+    navigate(`/movieDetails`, { state: { movieId: id } });
+  // const {
+  //   isLoading,
+  //   error,
+  //   data: movieDetails,
+  // } = useQuery(["searchById", imdbID], () => searchById(imdbID), {
+  //   staleTime: Infinity,
+  //   cacheTime: Infinity,
+  // });
 
   return (
-    <div class="flex flex-col items-center py-4 bg-amber-500 w-80 mb-4 rounded-lg border border-gray-200 shadow-md hover:bg-amber-200">
-      <h2 className="mb-1 font-bold text-center">{`${Title} (${Year})`}</h2>
+    <div class="flex flex-col items-center py-2 bg-amber-500 w-80 mb-4 rounded-lg border border-gray-200 shadow-md hover:bg-amber-200">
+      <h2 className="mb-1 font-bold text-center">{`${title} ${description}`}</h2>
       <img
-        src={Poster}
+        src={image}
         alt="Poster"
         class="rounded h-72 w-60 content-center mb-1"
       />
-      {movieDetails?.data?.Ratings?.length && (
-        <RatingsComponent ratings={movieDetails.data.Ratings} />
+      {imDbRating && (
+        <RatingsComponent
+          ratings={[
+            {
+              source: "Internet Movie Database",
+              value: imDbRating,
+              votes: imDbRatingVotes,
+            },
+          ]}
+        />
       )}
+      <button
+        onClick={handleClickView}
+        type="button"
+        class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mt-2"
+      >
+        View
+      </button>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,5 +3,11 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    // ...
+  ],
 };
